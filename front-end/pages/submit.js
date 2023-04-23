1a739ec9fa7406554f160fd149df04c7200a2aee
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-04-23 11:32:36 +0300

    added TimelineSection component

--- front-end/pages/submit.js
+++ front-end/pages/submit.js
@@ -1,11 +1,13 @@
 import BasicSection from "../components/BasicSection";
 import BudgetSection from "../components/BudgetSection";
+import TimelineSection from "../components/TimelineSection";
 
 function Submit() {
     return (
         <>
             <BasicSection></BasicSection>
             <BudgetSection></BudgetSection>
+            <TimelineSection></TimelineSection>
         </>
     )
 }
