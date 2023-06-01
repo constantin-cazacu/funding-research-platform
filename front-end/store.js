a726d5915d393e5e9a620e9895cc474b79a629df
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-06-01 19:39:33 +0300

    tested redux functionality on rendering components based on storing JWT and user role

--- front-end/store.js
+++ front-end/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from 'slices/authSlice';
 
 const store = configureStore({
   reducer: {
     // Add your reducers here
+    auth: authReducer,
   },
 });
 
