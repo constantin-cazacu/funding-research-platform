f0f337fc3c645e31b3835fcee4f8761f1b928b68
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-06-03 00:36:37 +0300

    fixed missing props

--- front-end/components/ProjectTabs.js
+++ front-end/components/ProjectTabs.js
@@ -1,78 +1,98 @@
+import { useState } from 'react';
 import { Box, Tab, Tabs, Typography } from '@mui/material';
 import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineDot } from '@mui/lab';
-import { Doughnut } from 'react-chartjs-2';
+import { VictoryPie } from 'victory';
 
 const ProjectTabs = ({ abstract, budget_items, timeline_items }) => {
+  const [selectedTab, setSelectedTab] = useState(0);
+
   const handleChange = (event, newValue) => {
-    // Handle tab change if needed
+    setSelectedTab(newValue);
   };
 
   const renderBudgetChart = () => {
-    const labels = budget_items.map(item => item.budget_item);
-    const data = budget_items.map(item => item.sum);
-    const chartData = {
-      labels,
-      datasets: [
-        {
-          data,
-          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'], // Customize the colors as needed
-        },
-      ],
-    };
+    const data = budget_items.map(item => ({
+      x: item.name,
+      y: parseInt(item.sum),
+    }));
 
     return (
       <Box width={200} height={200}>
-        <Doughnut data={chartData} />
+        <VictoryPie
+          data={data}
+          innerRadius={100}
+          colorScale={['#FF6384', '#36A2EB', '#FFCE56']} // Customize the colors as needed
+        />
       </Box>
     );
   };
 
   return (
     <Box>
-      <Tabs value={0} onChange={handleChange} indicatorColor="primary" textColor="primary">
+      <Tabs value={selectedTab} onChange={handleChange} indicatorColor="primary" textColor="primary">
         <Tab label="Overview" />
         <Tab label="Updates" />
         <Tab label="FAQ" />
       </Tabs>
       <Box mt={2}>
-        <Box>
-          <Typography variant="h6" gutterBottom>
-            About
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            {abstract}
-          </Typography>
-        </Box>
-        <Box mt={2}>
-          <Typography variant="h6" gutterBottom>
-            Budget
-          </Typography>
-          {budget_items.map((item, index) => (
-            <Typography key={index} variant="body1" gutterBottom>
-              {`${item.budget_item}: $${item.sum}`}
+        {selectedTab === 0 && (
+          <Box>
+            <Typography variant="h6" gutterBottom>
+              About
             </Typography>
-          ))}
-          {renderBudgetChart()}
-        </Box>
-        <Box mt={2}>
-          <Typography variant="h6" gutterBottom>
-            Timeline
-          </Typography>
-          <Timeline>
-            {timeline_items.map((item, index) => (
-              <TimelineItem key={index}>
-                <TimelineSeparator>
-                  <TimelineDot />
-                  {index !== timeline_items.length - 1 && <TimelineConnector />}
-                </TimelineSeparator>
-                <TimelineContent>
-                  <Typography variant="body1">{item.timeline_item}</Typography>
-                  <Typography variant="body2" color="textSecondary">{item.date}</Typography>
-                </TimelineContent>
-              </TimelineItem>
+            <Typography variant="body1" gutterBottom>
+              {abstract}
+            </Typography>
+
+            <Typography variant="h6" gutterBottom>
+              Budget
+            </Typography>
+            {budget_items.map((item, index) => (
+              <Typography key={index} variant="body1" gutterBottom>
+                {`${item.name}: $${item.sum}`}
+              </Typography>
             ))}
-          </Timeline>
-        </Box>
+            {renderBudgetChart()}
+
+            <Typography variant="h6" gutterBottom>
+              Timeline
+            </Typography>
+            <Timeline>
+              {timeline_items.map((item, index) => (
+                <TimelineItem key={index}>
+                  <TimelineSeparator>
+                    <TimelineDot />
+                    {index !== timeline_items.length - 1 && <TimelineConnector />}
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant="body1">{item.name}</Typography>
+                    <Typography variant="body2" color="textSecondary">{item.date}</Typography>
+                  </TimelineContent>
+                </TimelineItem>
+              ))}
+            </Timeline>
+          </Box>
+        )}
+        {selectedTab === 1 && (
+          <Box>
+            <Typography variant="h6" gutterBottom>
+              Updates
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Here will be updates
+            </Typography>
+          </Box>
+        )}
+        {selectedTab === 2 && (
+          <Box>
+            <Typography variant="h6" gutterBottom>
+              FAQ
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Here are some frequently asked questions
+            </Typography>
+          </Box>
+        )}
       </Box>
     </Box>
   );
