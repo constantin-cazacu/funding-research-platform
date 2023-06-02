a68fe1aa09fac987cf344ad775220fcc0553e630
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-06-02 22:46:13 +0300

    Create FundingProgress.js

---
+++ front-end/components/FundingProgress.js
@@ -0,0 +1,20 @@
+import { Box, LinearProgress, Typography } from '@mui/material';
+
+const FundingProgress = ({ collected_funds, funding_goal }) => {
+  const progress = (collected_funds / funding_goal) * 100;
+
+  return (
+    <Box>
+      <Typography variant="subtitle1" gutterBottom>
+        Funding Progress
+      </Typography>
+      <LinearProgress variant="determinate" value={progress} />
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="caption">{`Collected Funds: $${collected_funds}`}</Typography>
+        <Typography variant="caption">{`Funding Goal: $${funding_goal}`}</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default FundingProgress;
