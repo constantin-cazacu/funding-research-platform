bd63e88a61efcd5b13f0953376b1e5dc9e0c9e49
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-06-03 00:30:28 +0300

    fixed missing request issue

--- front-end/components/ProjectPage.js
+++ front-end/components/ProjectPage.js
@@ -3,17 +3,27 @@ import Navbar from './Navbar';
 import ProjectInfoBox from './ProjectInfoBox';
 import ProjectTabs from './ProjectTabs';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ProjectPage = ({ projectId }) => {
   const [projectData, setProjectData] = useState(null);
 
   useEffect(() => {
-    // Fetch project data based on the projectId from the backend API
-    // Replace 'api/projects/${projectId}' with the actual endpoint URL
-    fetch(`http://localhost:5001/api/projects/${projectId}`)
-      .then((response) => response.json())
-      .then((data) => setProjectData(data))
-      .catch((error) => console.error(error));
+    const fetchProjectData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/project_data`, {
+          params: {
+            id : projectId
+          }
+        });
+        setProjectData(response.data.data);
+        console.log(response.data.data)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProjectData();
   }, [projectId]);
 
   if (!projectData) {
@@ -21,7 +31,17 @@ const ProjectPage = ({ projectId }) => {
     return <div>Loading...</div>;
   }
 
-  const { project_name, owner, image_url, abstract, fields_of_study, budget_items, timeline_items, collected_funds, funding_goal } = projectData;
+  const {
+    title: project_name,
+    owner,
+    image_url,
+    abstract,
+    fields_of_study,
+    budget: budget_items, // Rename the property to budget_items
+    timeline: timeline_items, // Rename the property to timeline_items
+    collected_funds,
+    funding_goal
+  } = projectData;
 
   return (
     <Box>
