94c789f811a2dbf0b588808bac417d53319a7d09
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-06-05 10:23:50 +0300

    Create BusinessProjectInfoBox.js

---
+++ front-end/components/BusinessProjectInfoBox.js
@@ -0,0 +1,21 @@
+import { Box, Chip, Typography, Button } from '@mui/material';
+
+const BusinessProjectInfoBox = ({ budget, fields_of_study }) => {
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" p={2}>
+      <Typography variant="h5" gutterBottom>
+        {`$${budget}`}
+      </Typography>
+      <Box display="flex" justifyContent="flex-start" marginBottom={1}>
+        {fields_of_study.slice(0, 2).map((field, index) => (
+          <Chip key={index} label={field} size="small" />
+        ))}
+      </Box>
+      <Button variant="contained" color="primary">
+        Apply for project
+      </Button>
+    </Box>
+  );
+};
+
+export default BusinessProjectInfoBox;
