0c14c13508637c517cc3a6448b7650711d7b0084
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2023-06-05 19:37:13 +0300

    added owner name and currency

--- front-end/components/ResearcherProjectPage.js
+++ front-end/components/ResearcherProjectPage.js
@@ -4,6 +4,7 @@ import ProjectInfoBox from './ProjectInfoBox';
 import ProjectTabs from './ProjectTabs';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import projectImage from '../public/bg-aperiodic-tilings.jpg';
 
 const ResearcherProjectPage = ({ projectId }) => {
   const [projectData, setProjectData] = useState(null);
@@ -33,14 +34,16 @@ const ResearcherProjectPage = ({ projectId }) => {
 
   const {
     title: project_name,
-    owner,
+    student,
+    supervisor,
     image_url,
     abstract,
     fields_of_study,
     budget: budget_items, // Rename the property to budget_items
     timeline: timeline_items, // Rename the property to timeline_items
     collected_funds,
-    funding_goal
+    funding_goal,
+    currency
   } = projectData;
 
   return (
@@ -51,16 +54,20 @@ const ResearcherProjectPage = ({ projectId }) => {
           {project_name}
         </Typography>
         <Typography variant="subtitle1" gutterBottom>
-          Owner: {owner}
+          Student: {student}
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom>
+          Supervisor: {supervisor}
         </Typography>
         <Grid container spacing={2} mt={2}>
           <Grid item xs={12} sm={8}>
-            <img src={image_url} alt="Project" style={{ width: '100%', height: 'auto' }} />
+            <img src={projectImage.src} alt="Project" style={{ width: '100%', height: 'auto' }} />
           </Grid>
           <Grid item xs={12} sm={4}>
             <ProjectInfoBox
               collected_funds={collected_funds}
               funding_goal={funding_goal}
+              currency={currency}
               fields_of_study={fields_of_study}
             />
           </Grid>
@@ -69,6 +76,7 @@ const ResearcherProjectPage = ({ projectId }) => {
           abstract={abstract}
           budget_items={budget_items}
           timeline_items={timeline_items}
+          currency={currency}
         />
       </Container>
     </Box>
